54ed3c7a61d4bddf9214b840ff6ad63c3a2ca533
prin-r <q@bandprotocol.com>
2023-07-25 18:51:46 +0700

    fix challenge

--- test/FutureBridge.t.sol
+++ test/FutureBridge.t.sol
@@ -9,11 +9,13 @@ contract FutureBridgeTest is Test {
 	// secp256k1 group order
 	uint256 public constant ORDER = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
 	bytes32 public constant UPDATE_KEY_OPERATION_HASH = keccak256(bytes("UPDATE_KEY_OPERATION_HASH"));
+	uint256 _privateKey = uint256(keccak256(abi.encodePacked("TEST_PRIVATE_KEY")));
 
 	FutureBridge public so;
 
 	function setUp() public {
-		so = new FutureBridge(27, 0xe3e216b180a2a8ec316d500faa7a44c43a87f8adcb54c553c6277d3e220a40c8);
+		(uint8 parity, uint256 px) = getPubkey(_privateKey);
+		so = new FutureBridge(parity, px);
 	}
 
 	function challenge(
@@ -22,7 +24,13 @@ contract FutureBridgeTest is Test {
 		uint256 _px,
 		bytes32 messageHash
 	) public pure returns (uint256 c) {
-		c = uint256(keccak256(abi.encodePacked(rAddress, _parity, _px, messageHash)));
+		c = uint256(keccak256(abi.encodePacked(
+            bytes32(0x42414e442d5453532d736563703235366b312d7630006368616c6c656e676500),
+            rAddress,
+            _parity,
+            _px,
+            messageHash
+        )));
 	}
 
 	function getPubkey(uint256 privateKey) public pure returns (uint8 parity, uint256 px) {
@@ -34,14 +42,17 @@ contract FutureBridgeTest is Test {
 		}
 	}
 
+	function randomK(uint256 privateKey) public pure returns(uint256 k) {
+		k = uint256(keccak256(abi.encodePacked("salt", privateKey)));
+	}
+
 	function schnorrSign(
 		uint8 parity,
 		uint256 px,
+		uint256 k,
 		bytes32 messageHash,
 		uint256 privateKey
-	) public pure returns (address rAddress, uint256 s) {
-		// R = G * k
-		uint256 k = uint256(keccak256(abi.encodePacked("salt", privateKey)));
+	) public view returns (address rAddress, uint256 s) {
 		rAddress = vm.addr(k);
 
 		// c = h(address(R) || compressed pubkey || m)
@@ -67,7 +78,7 @@ contract FutureBridgeTest is Test {
 
 	function test_cal_challenge() public {
 		assertEq(
-			0x9579241dedad1ae4a2d944c22be86421de9f4962c0f5d3703e51cb1a19d1015b,
+			0x0c0753165ef3bd86580a99a06e89577319e79de82cb6b5ebf7e95ffc6204d118,
 			challenge(
 				27,
 				address(0x8B54b734034523969E24d0107fa920744a8C97D6),
@@ -76,12 +87,21 @@ contract FutureBridgeTest is Test {
 			)
 		);
 		assertEq(
-			0xfaa5c6ded8167ea3d72af6dcfc589d2bcebc1a9883acc393cdde502e6ba11c5b,
+			0xe1540ce356e232409afec4a0ffc972a516d648d2a9dbc9acbb80af4e0a588cd3,
 			challenge(
 				27,
 				address(0x225743E6566bE4Faa0F834bdA86b60FD0b9cF33d),
 				0x258ea24862d6ff3c337cc125e8efefa87fb9d178fee0cf843afb1ae389a6db53,
 				0x97acf51be6080597a20fae668f1161416f21f89963de35ece00b4da4d6bafd4e
+			)
+		);
+		assertEq(
+			0x4bcd43e95085f3593a3213bddaeb1de7357e0bea1ad16a90c26ece31e6fd3e79,
+			challenge(
+				27,
+				address(0x4991030Be950BE3dD902612585Ff64a1e5d017ec),
+				0xa6ffbcd81f51883fd64cda2bdc3150fe50ccbe27f482201467d4e7a7f1a5212e,
+				0xdc72d2ddbfa8bdafa3984c7884dd2fd9af7b82e2128711b1c5c508b443cf3bb4
 			)
 		);
 	}
@@ -108,55 +128,58 @@ contract FutureBridgeTest is Test {
 	}
 
 	function test_schnorrSign() public {
-		(uint8 parity, uint256 px) = getPubkey(0x0476e639d864bd63f33c5bd8be3f5310fe3aa2768e934b8619b63e3cb6f8f9dd);
+		uint256 secret = 0x383b4a7a98c26e381cf5149dade39a61b3f43e1af4ff1f78d17daf9ffaf35177;
+		(uint8 parity, uint256 px) = getPubkey(secret);
 		(address rAddress, uint256 s) = schnorrSign(
 			parity,
 			px,
-			hex"d63998219b321a434750448bdf5d7474f30befcf57157346dc3af1839a43d666",
-			0x0476e639d864bd63f33c5bd8be3f5310fe3aa2768e934b8619b63e3cb6f8f9dd
+			0x427d0cfec0deb6332e7961cf387a8668730cda6f5b99c2821128dab11a5c362e,
+			hex"dc72d2ddbfa8bdafa3984c7884dd2fd9af7b82e2128711b1c5c508b443cf3bb4",
+			secret
 		);
-		assertEq(uint8(28), parity);
-		assertEq(0x83a38b88ec6fb48b4ade03b58f6a298b7b45da6dd9468ffdc348331ac9784189, px);
-		assertEq(address(0xFbc4Ce7C877858f60AD33fcE77Fda71f864FE82E), rAddress);
-		assertEq(0x1cfcbb7275284f780b1829f4c5f263bc513f3f2e12ccb68f28da09d0e2d48ac5, s);
+		assertEq(uint8(27), parity);
+		assertEq(px, 0xa6ffbcd81f51883fd64cda2bdc3150fe50ccbe27f482201467d4e7a7f1a5212e);
+		assertEq(rAddress, address(0x4991030Be950BE3dD902612585Ff64a1e5d017ec));
+		assertEq(s, 0xc8727773e662c4c23db072ce0ce27b1ffe3f4ee8a93460efdf61b5a9af5edffd);
 
-		(parity, px) = getPubkey(0x0be8520e9b67260bd7ec68c9d937a2ae0e11efee528dda48c6b1ff38e21588b2);
+		secret = 0x05cff511ca26b944fb22285c8a56595baeefe399d49e5aeef27233701b59b8ad;
+		(parity, px) = getPubkey(secret);
 		(rAddress, s) = schnorrSign(
 			parity,
 			px,
-			hex"3c740a1734c898f7ab1f9889e00ac96a717da51c74977086b436cf7f02bade24",
-			0x0be8520e9b67260bd7ec68c9d937a2ae0e11efee528dda48c6b1ff38e21588b2
+			0x419d15a75ccf9809027431b7b42cf1e0b2403eeca686fdca8497f8309b62d363,
+			hex"661cde0d0f34a8c975409889bdc9d78e3b0e6c1d66997b03989d1ae3bfd14fc3",
+			secret
 		);
-		assertEq(uint8(27), parity);
-		assertEq(0xc9514cb0e4fb09a827ca14401f7b254681dca69f464e948563e4874ada83fda6, px);
-		assertEq(address(0x0a42bae6401fFC346c4e7Ba29FB0b1E51Ef7026D), rAddress);
-		assertEq(0xb57cc365b16318dfd2e451836bc6763a42319a009178469eac3304f85ef6b7a0, s);
+		assertEq(uint8(28), parity);
+		assertEq(px, 0xa221bfe76ca3ae50df876afda2701cb1cad0527e28d0680f8af00ce14e6b079a);
+		assertEq(rAddress, address(0x2F7f7613Ab95bff1a6B23F3AFD3fA6dd80F04461));
+		assertEq(s, 0xefb10cb564c33dca7d57a697941f4d182f5e80fa351a20ed5538453d6d8c2393);
 	}
 
 	function test_verify() public {
-		uint256 numRound = 100;
-		uint256 gasUsedVerifyAcc = 0;
-		uint256 gasUsedUpdateAcc = 0;
-		bytes32 seed = keccak256(abi.encode("initial_random_seed"));
-		uint256 privateKey = uint256(keccak256(abi.encode(seed, "privateKey")));
+		uint256 gasUsedVerifyAcc;
+		uint256 gasUsedUpdateAcc;
+		uint256 nextPrivateKey;
+		uint256 privateKey = _privateKey;
+		uint256 start;
 
-		for (uint256 i = 0; i < numRound; i++) {
-			bytes32 anyMessageHash = keccak256(abi.encode(seed, "any message to be sign"));
+		for (uint256 i = 0; i < 100; i++) {
+			bytes32 anyMessageHash = keccak256(abi.encodePacked(i, "any message to be sign"));
 			(uint8 parity, uint256 px) = getPubkey(privateKey);
-			(address rAddress, uint256 s) = schnorrSign(parity, px, anyMessageHash, privateKey);
+			(address rAddress, uint256 s) = schnorrSign(parity, px, randomK(privateKey), anyMessageHash, privateKey);
 
-			uint256 start = gasleft();
+			start = gasleft();
 			// verify signature
-			assertEq(true, so.verify(rAddress, s, anyMessageHash));
+			assertEq(so.verify(rAddress, s, anyMessageHash), true);
 			gasUsedVerifyAcc += start - gasleft();
 
-			// calculate variables for the next round
-			seed = keccak256(abi.encode(seed, i));
+			nextPrivateKey = uint256(keccak256(abi.encodePacked(i, privateKey, "next privateKey")));
 
 			// update pubkey
-			(uint8 newParity, uint256 newPx) = getPubkey(uint256(keccak256(abi.encode(seed, "next privateKey"))));
-			bytes32 messageHash = keccak256(abi.encode(UPDATE_KEY_OPERATION_HASH, newParity, newPx));
-			(rAddress, s) = schnorrSign(parity, px, messageHash, privateKey);
+			(uint8 newParity, uint256 newPx) = getPubkey(nextPrivateKey);
+			bytes32 messageHash = keccak256(abi.encodePacked(UPDATE_KEY_OPERATION_HASH, newParity, newPx));
+			(rAddress, s) = schnorrSign(parity, px, randomK(privateKey), messageHash, privateKey);
 
 			start = gasleft();
 			so.updatePubkey(newParity, rAddress, newPx, s);
@@ -165,7 +188,7 @@ contract FutureBridgeTest is Test {
 			assertEq(so.parity(), newParity);
 			assertEq(so.px(), newPx);
 
-			privateKey = uint256(keccak256(abi.encode(seed, "next privateKey")));
+			privateKey = nextPrivateKey;
 
 			if (i == 0) {
 				console.log("initial verify gas avg = ", gasUsedVerifyAcc);
@@ -173,7 +196,7 @@ contract FutureBridgeTest is Test {
 			}
 		}
 
-		console.log("verify gas avg = ", gasUsedVerifyAcc / numRound);
-		console.log("update pubkey gas avg = ", gasUsedUpdateAcc / numRound);
+		console.log("verify gas avg = ", gasUsedVerifyAcc / 100);
+		console.log("update pubkey gas avg = ", gasUsedUpdateAcc / 100);
 	}
 }

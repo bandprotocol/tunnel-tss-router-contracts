8c7c98d2bd08d1d76cdf16106c0bca9fd0027889
nkitlabs <natthakunk0109@gmail.com>
2024-11-29 08:35:29 +0700

    format helpers

--- test/helper/SECP256k1.sol
+++ test/helper/SECP256k1.sol
@@ -10,12 +10,9 @@ License: GPL-3.0
 
 // This library was included solely for testing purposes and is not a component of FutureBridge.
 library SECP256k1 {
-    uint256 public constant GX =
-        0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
-    uint256 public constant GY =
-        0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8;
-    uint256 public constant N =
-        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
+    uint256 public constant GX = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
+    uint256 public constant GY = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8;
+    uint256 public constant N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
     uint256 public constant A = 0;
     uint256 public constant B = 7;
 
@@ -23,45 +20,19 @@ library SECP256k1 {
         return N;
     }
 
-    function _jAdd(
-        uint256 x1,
-        uint256 z1,
-        uint256 x2,
-        uint256 z2
-    ) public pure returns (uint256 x3, uint256 z3) {
-        (x3, z3) = (
-            addmod(mulmod(z2, x1, N), mulmod(x2, z1, N), N),
-            mulmod(z1, z2, N)
-        );
+    function _jAdd(uint256 x1, uint256 z1, uint256 x2, uint256 z2) public pure returns (uint256 x3, uint256 z3) {
+        (x3, z3) = (addmod(mulmod(z2, x1, N), mulmod(x2, z1, N), N), mulmod(z1, z2, N));
     }
 
-    function _jSub(
-        uint256 x1,
-        uint256 z1,
-        uint256 x2,
-        uint256 z2
-    ) public pure returns (uint256 x3, uint256 z3) {
-        (x3, z3) = (
-            addmod(mulmod(z2, x1, N), mulmod(N - x2, z1, N), N),
-            mulmod(z1, z2, N)
-        );
+    function _jSub(uint256 x1, uint256 z1, uint256 x2, uint256 z2) public pure returns (uint256 x3, uint256 z3) {
+        (x3, z3) = (addmod(mulmod(z2, x1, N), mulmod(N - x2, z1, N), N), mulmod(z1, z2, N));
     }
 
-    function _jMul(
-        uint256 x1,
-        uint256 z1,
-        uint256 x2,
-        uint256 z2
-    ) public pure returns (uint256 x3, uint256 z3) {
+    function _jMul(uint256 x1, uint256 z1, uint256 x2, uint256 z2) public pure returns (uint256 x3, uint256 z3) {
         (x3, z3) = (mulmod(x1, x2, N), mulmod(z1, z2, N));
     }
 
-    function _jDiv(
-        uint256 x1,
-        uint256 z1,
-        uint256 x2,
-        uint256 z2
-    ) public pure returns (uint256 x3, uint256 z3) {
+    function _jDiv(uint256 x1, uint256 z1, uint256 x2, uint256 z2) public pure returns (uint256 x3, uint256 z3) {
         (x3, z3) = (mulmod(x1, z2, N), mulmod(z1, x2, N));
     }
 
@@ -81,14 +52,11 @@ library SECP256k1 {
         return t;
     }
 
-    function _ecAdd(
-        uint256 x1,
-        uint256 y1,
-        uint256 z1,
-        uint256 x2,
-        uint256 y2,
-        uint256 z2
-    ) public pure returns (uint256 x3, uint256 y3, uint256 z3) {
+    function _ecAdd(uint256 x1, uint256 y1, uint256 z1, uint256 x2, uint256 y2, uint256 z2)
+        public
+        pure
+        returns (uint256 x3, uint256 y3, uint256 z3)
+    {
         uint256 lx;
         uint256 lz;
         uint256 da;
@@ -132,20 +100,15 @@ library SECP256k1 {
         }
     }
 
-    function _ecDouble(
-        uint256 x1,
-        uint256 y1,
-        uint256 z1
-    ) public pure returns (uint256 x3, uint256 y3, uint256 z3) {
+    function _ecDouble(uint256 x1, uint256 y1, uint256 z1) public pure returns (uint256 x3, uint256 y3, uint256 z3) {
         (x3, y3, z3) = _ecAdd(x1, y1, z1, x1, y1, z1);
     }
 
-    function _ecMul(
-        uint256 d,
-        uint256 x1,
-        uint256 y1,
-        uint256 z1
-    ) public pure returns (uint256 x3, uint256 y3, uint256 z3) {
+    function _ecMul(uint256 d, uint256 x1, uint256 y1, uint256 z1)
+        public
+        pure
+        returns (uint256 x3, uint256 y3, uint256 z3)
+    {
         uint256 remaining = d;
         uint256 px = x1;
         uint256 py = y1;
@@ -169,12 +132,7 @@ library SECP256k1 {
         (x3, y3, z3) = (acx, acy, acz);
     }
 
-    function ecadd(
-        uint256 x1,
-        uint256 y1,
-        uint256 x2,
-        uint256 y2
-    ) public pure returns (uint256 x3, uint256 y3) {
+    function ecadd(uint256 x1, uint256 y1, uint256 x2, uint256 y2) public pure returns (uint256 x3, uint256 y3) {
         uint256 z;
         (x3, y3, z) = _ecAdd(x1, y1, 1, x2, y2, 1);
         z = _inverse(z);
@@ -182,11 +140,7 @@ library SECP256k1 {
         y3 = mulmod(y3, z, N);
     }
 
-    function ecmul(
-        uint256 x1,
-        uint256 y1,
-        uint256 scalar
-    ) public pure returns (uint256 x2, uint256 y2) {
+    function ecmul(uint256 x1, uint256 y1, uint256 scalar) public pure returns (uint256 x2, uint256 y2) {
         uint256 z;
         (x2, y2, z) = _ecMul(scalar, x1, y1, 1);
         z = _inverse(z);
@@ -195,70 +149,42 @@ library SECP256k1 {
     }
 
     function pointHash(uint256[2] memory point) public pure returns (address) {
-        return
-            address(
-                uint160(
-                    uint256(keccak256(abi.encodePacked(point[0], point[1]))) &
-                        0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
-                )
-            );
+        return address(
+            uint160(
+                uint256(keccak256(abi.encodePacked(point[0], point[1]))) & 0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+            )
+        );
     }
 
     /**
      * hash(g^a + B^c)
      */
-    function sbmulAddMul(
-        uint256 s,
-        uint256[2] memory b,
-        uint256 c
-    ) public pure returns (address) {
+    function sbmulAddMul(uint256 s, uint256[2] memory b, uint256 c) public pure returns (address) {
         uint256 q = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
         s = (q - s) % q;
         s = mulmod(s, b[0], q);
 
-        return
-            ecrecover(
-                bytes32(s),
-                b[1] % 2 != 0 ? 28 : 27,
-                bytes32(b[0]),
-                bytes32(mulmod(c, b[0], q))
-            );
+        return ecrecover(bytes32(s), b[1] % 2 != 0 ? 28 : 27, bytes32(b[0]), bytes32(mulmod(c, b[0], q)));
     }
 
     //
     // Based on the original idea of Vitalik Buterin:
     // https://ethresear.ch/t/you-can-kinda-abuse-ecrecover-to-do-ecmul-in-secp256k1-today/2384/9
     //
-    function ecmulVerify(
-        uint256 x1,
-        uint256 y1,
-        uint256 scalar,
-        uint256 qx,
-        uint256 qy
-    ) public pure returns (bool) {
+    function ecmulVerify(uint256 x1, uint256 y1, uint256 scalar, uint256 qx, uint256 qy) public pure returns (bool) {
         address signer = sbmulAddMul(0, [x1, y1], scalar);
         return pointHash([qx, qy]) == signer;
     }
 
-    function publicKey(
-        uint256 privKey
-    ) public pure returns (uint256 qx, uint256 qy) {
+    function publicKey(uint256 privKey) public pure returns (uint256 qx, uint256 qy) {
         return ecmul(GX, GY, privKey);
     }
 
-    function publicKeyVerify(
-        uint256 privKey,
-        uint256 x,
-        uint256 y
-    ) public pure returns (bool) {
+    function publicKeyVerify(uint256 privKey, uint256 x, uint256 y) public pure returns (bool) {
         return ecmulVerify(GX, GY, privKey, x, y);
     }
 
-    function deriveKey(
-        uint256 privKey,
-        uint256 pubX,
-        uint256 pubY
-    ) public pure returns (uint256 qx, uint256 qy) {
+    function deriveKey(uint256 privKey, uint256 pubX, uint256 pubY) public pure returns (uint256 qx, uint256 qy) {
         uint256 z;
         (qx, qy, z) = _ecMul(privKey, pubX, pubY, 1);
         z = _inverse(z);

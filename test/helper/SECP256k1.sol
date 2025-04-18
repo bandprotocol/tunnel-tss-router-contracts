841971cc570f8bf9b4a6a725beaf215c92370e27
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2025-04-18 16:25:22 +0700

    remove old wording

--- test/helper/SECP256k1.sol
+++ test/helper/SECP256k1.sol
@@ -8,7 +8,7 @@ Taken from https://github.com/jbaylina/ecsol and https://github.com/1Address/ecs
 License: GPL-3.0
 */
 
-// This library was included solely for testing purposes and is not a component of FutureBridge.
+// This library was included solely for testing purposes.
 library SECP256k1 {
     uint256 public constant GX = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
     uint256 public constant GY = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8;

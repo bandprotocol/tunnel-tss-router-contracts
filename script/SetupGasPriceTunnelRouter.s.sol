0f757b7c727e272db460541768c0995e8e7437d0
Natthakun <natthakun@Natthakuns-MacBook-Air.local>
2025-07-18 16:28:34 +0700

    fix script

--- script/SetupGasPriceTunnelRouter.s.sol
+++ script/SetupGasPriceTunnelRouter.s.sol
@@ -13,7 +13,7 @@ import {GasPriceTunnelRouter} from "../src/router/GasPriceTunnelRouter.sol";
 import {TssVerifier} from "../src/TssVerifier.sol";
 import {Vault} from "../src/Vault.sol";
 
-contract Executer is Script {
+contract Executor is Script {
     function run() external {
         uint64 transitionPeriod = uint64(vm.envUint("TRANSITION_PERIOD"));
         bytes32 transitionOriginatorHash = bytes32(
@@ -78,21 +78,21 @@ contract Executer is Script {
             proxyTunnelRouterAddr
         );
 
-        // Set the tunnel router address in the vault
-        Vault(payable(proxyVaultAddr)).setTunnelRouter(proxyTunnelRouterAddr);
+        // // Set the tunnel router address in the vault
+        // Vault(payable(proxyVaultAddr)).setTunnelRouter(proxyTunnelRouterAddr);
 
-        vm.stopBroadcast();
+        // vm.stopBroadcast();
 
-        console.log("Vault Proxy deployed at:", proxyVaultAddr);
-        console.log("Vault Implementation deployed at:", implVaultAddr);
-        console.log("TssVerifier deployed at:", address(tssVerifier));
-        console.log(
-            "GasPriceTunnelRouter Proxy deployed at:",
-            proxyTunnelRouterAddr
-        );
-        console.log(
-            "GasPriceTunnelRouter Implementation deployed at:",
-            implTunnelRouterAddr
-        );
+        // console.log("Vault Proxy deployed at:", proxyVaultAddr);
+        // console.log("Vault Implementation deployed at:", implVaultAddr);
+        // console.log("TssVerifier deployed at:", address(tssVerifier));
+        // console.log(
+        //     "GasPriceTunnelRouter Proxy deployed at:",
+        //     proxyTunnelRouterAddr
+        // );
+        // console.log(
+        //     "GasPriceTunnelRouter Implementation deployed at:",
+        //     implTunnelRouterAddr
+        // );
     }
 }
